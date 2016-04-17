fd3bd7c527ec9d64e3a22a12d8ef32f101ead703
SerxLee <serx.lee@gmail.com>
2016-04-17 15:46:54 +0800

    add download

--- ViewController.swift
+++ ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 
 class ViewController: UIViewController, NSURLSessionDelegate, NSURLSessionDataDelegate,  NSURLSessionTaskDelegate{
-
+    
+    
+    //MARK: - ------All Properties------
+    //MARK: -
+    
     var imageView: UIImageView = {
         let imageView = UIImageView(frame: CGRect(x: 40, y: 40, width: 300, height: 300))
         imageView.backgroundColor = UIColor.lightGrayColor()
@@ -18,8 +22,13 @@ class ViewController: UIViewController, NSURLSessionDelegate, NSURLSessionDataDe
         return imageView
     }()
     
-    var session = NSURLSession()
+     //定义一个默认的session
+    var defaultSession = NSURLSession()
+    
+    //获取data的task
     var dataTast: NSURLSessionDataTask = NSURLSessionDataTask()
+    
+    //进度条
     var progressview = UIProgressView()
     
     var expectedLengh = Int64()
@@ -28,47 +37,50 @@ class ViewController: UIViewController, NSURLSessionDelegate, NSURLSessionDataDe
     let imageUrl:NSURL = NSURL(string: "http://pics.sc.chinaz.com/files/pic/pic9/201509/apic15014.jpg")!
     
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    //下载 session ， downloadsession
+    lazy var downLoadSession: NSURLSession = {
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
         
+        //delegateQueue设置为nil session会自己创建一个串行的队列
+        let session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+        
+        return session
+    }()
+    
+
+    //MARK: - ----Apple Inc.Func----
+    //MARK: -
+    
+        override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        doTest2()
+            
+        /*
+        downloadEpisodeWithFeedItem()
+        dataTestEpisodeWithFeedItem(imageUrl)
+        
+        self.view.addSubview(imageView)
+
+            
         self.dataTast.resume()
-        self.session.finishTasksAndInvalidate()
+        self.defaultSession.finishTasksAndInvalidate()
+        */
         
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        downloadEpisodeWithFeedItem()
-        dataTestEpisodeWithFeedItem(imageUrl)
-        
-        self.view.addSubview(imageView)
-    }
-    
-    func dataTestEpisodeWithFeedItem(episodeURL: NSURL){
-        
-        
-        
-//        let request: NSURLRequest = NSURLRequest(URL: episodeURL)
-        dataTast = session.dataTaskWithURL(episodeURL)
-        
-    }
-    
-    func downloadEpisodeWithFeedItem() {
-        
-        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
-        session = NSURLSession(configuration: config, delegate: self, delegateQueue: NSOperationQueue.mainQueue())
-        
-    }
 
-    override func didReceiveMemoryWarning() {
+        
+    }
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 
+    /*
     func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveResponse response: NSURLResponse, completionHandler: (NSURLSessionResponseDisposition) -> Void) {
         
         let lenght = response.expectedContentLength
@@ -86,12 +98,16 @@ class ViewController: UIViewController, NSURLSessionDelegate, NSURLSessionDataDe
             presentViewController(alert, animated: true, completion: nil)
         }
     }
+     */
     
+    
+    //获取已接受数据， 计算百分 进度条
     func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData) {
         self.buffer.appendData(data)
         self.progressview.progress = Float(self.buffer.length)/Float(self.expectedLengh)
     }
     
+    //获取结束， check error
     func URLSession(session: NSURLSession, didBecomeInvalidWithError error: NSError?) {
         
         if (error == nil){
@@ -125,9 +141,54 @@ class ViewController: UIViewController, NSURLSessionDelegate, NSURLSessionDataDe
         }
     }
     
+    //MARK: - ----Individual Func----
+    //MARK: -
+    
+    //download executive
+    func downlaod(){
+        var downloadTask = NSURLSessionDataTask()
+        downloadTask = downLoadSession.dataTaskWithURL(imageUrl)
+        
+        downloadTask.resume()
+    }
+    
+    //defind the session by myself
+    func doTest2(){
+        
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        configuration.timeoutIntervalForRequest = 10.0
+        configuration.requestCachePolicy = .UseProtocolCachePolicy
+        configuration.HTTPAdditionalHeaders = ["Content-Type":"image/jpeg"]
+        
+        let defaultSession2 = NSURLSession(configuration: configuration)
+        
+        var dataTask2: NSURLSessionDataTask?
+        
+        let request = NSMutableURLRequest(URL: imageUrl)
+        
+        request.HTTPMethod = "GET"
+        
+        dataTask2 = defaultSession2.dataTaskWithRequest(request, completionHandler: { (data, response, error) in
+            
+            print(data)
+            print(response)
+        })
+        
+        dataTask2?.resume()
+    }
     
     
-
+    func dataTestEpisodeWithFeedItem(episodeURL: NSURL){
+//        let request: NSURLRequest = NSURLRequest(URL: episodeURL)
+        self.dataTast = self.defaultSession.dataTaskWithURL(episodeURL)
+    }
+    
+    func downloadEpisodeWithFeedItem() {
+        
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        defaultSession = NSURLSession(configuration: config, delegate: self, delegateQueue: NSOperationQueue.mainQueue())
+        
+    }
     
     @IBAction func resumeTest(sender: UIButton) {
         
